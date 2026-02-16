54f11874dd10275587e0a4d399f31ea0905c06db
SPSINGH3011 <spsingh302212@gmail.com>
2026-02-16 19:57:18 +0530

    add code

--- modules/azurerm_resource_group/output.tf
+++ modules/azurerm_resource_group/output.tf
@@ -1,3 +1,23 @@
-output "rg_ids" {
-  value = { for k, v in azurerm_resource_group.rgs : k => v.id }
+# output "rg_ids" {
+#   value = { for k, v in azurerm_resource_group.rgs : k => v.id }
+# }
+
+# output "resourcegroup_id" {
+#   value       = azurerm_resource_group_rgs.id
+# }
+
+output "rg-id" {
+  value = { for resourcegroups, rgs in azurerm_resource_group.rg : resourcegroups => rgs.id }
 }
+
+#for resourcegroup
+#resourcegroup    #variable block name   
+
+#rgs in azurerm_resource_group.rg
+#azurerm_resource_group.rgs     #Resorce Block address
+
+
+#resourcegroup => rgs.id 
+##for resourcegroup => #rgs in azurerm_resource_group.rg
+
+
